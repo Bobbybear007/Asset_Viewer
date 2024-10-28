2871df7cd18ba0478c21c4a66da423c8bc888f41
Andrew <andrewzambazos@gmail.com>
2024-10-29 09:42:28 +1300

    ue project deleted

--- Asset_Viewer_UE/Source/Asset_Viewer_UE/Asset_Viewer_UE.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Asset_Viewer_UE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Asset_Viewer_UE, "Asset_Viewer_UE" );
